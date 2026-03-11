1a73a451094c73a90e32560b8bc90924a35f9d02
field999satu <684259013@webmail.npru.ac.th>
2026-03-11 11:23:17 +0700

    4-3

---
+++ sql_inject_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Customer and Test SQL injection</title>
+</head>
+
+<body>
+    <h1>Test SQL injection</h1>
+    <form action="selection_injection.php" method="GET">
+        <input type="text" placeholder="Enter Customer ID" name="CustomerID">
+        <br> <br>
+        <input type="submit">
+
+    </form>
+</body>
+
+</html>
